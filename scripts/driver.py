8aa40f6cd0d35fae122d03d7008ce19a83322279
Barce <barce@me.com>
2017-11-02 22:22:15 -0700

    added update_secret, get_secret, get_keys

--- scripts/driver.py
+++ scripts/driver.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python
 
 from lockboxcli import LockboxClient
-client = LockboxClient()
+client = LockboxClient('staging')
 client.show_config()
-client.connect()
-client.environments()
-client.environment('test')
+resp = client.generate_config()
+print(resp)
+print(resp.text)
+var_list = client.get_keys()
+for var in var_list:
+    print(var)
 
+print("--- get secrets ---")
+r = client.get_secret('DJANGO_PASSWORD')
+print(r)
+
+r = client.get_secret('PIP_TEST')
+print(r)
+
+print("update PIP_TEST")
+r = client.update_secret('PIP_TEST', 'bazquux')
+print(r)
+print("get PIP_TEST")
+r = client.get_secret('PIP_TEST')
+print(r)
