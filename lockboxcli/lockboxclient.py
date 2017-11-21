640b698f613792a00faceb566147f707cc086ba2
Barce <james.barcelona@accuenmedia.com>
2017-11-21 15:03:19 -0800

    make key list unique

--- lockboxcli/lockboxclient.py
+++ lockboxcli/lockboxclient.py
@@ -85,7 +85,8 @@ class LockboxClient:
                     key_list.append(m.group(0).replace('$','').replace('{','').replace('}',''))
                 except:
                     foo = None
-        return key_list
+        set_list = set(key_list)
+        return set_list
 
     def get_secret(self, key):
         # is it in key_list?
